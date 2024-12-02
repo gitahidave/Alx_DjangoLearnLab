1edf3c17721f93fcf6f5221fcf49cc2bb3c8b552
David Gitahi <gitahidavid07@gmail.com>
2024-12-02 19:49:56 +0300

    Defined the Blog models

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User, AbstractUser, UserManager
+from django.conf import settings
 
 class Post(models.Model):
     title: models.CharField(max_length=200)
