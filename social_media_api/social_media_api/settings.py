39541810268f239dfb10c15391eef86fab72d3eb
David Gitahi <130385462+gitahidave@users.noreply.github.com>
2024-12-13 21:41:53 +0300

    Deploying the Django REST API to Production

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -137,3 +137,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+ALLOWED_HOSTS = ['your_domain.com', 'your_server_ip']
+"SECURE_BROWSER_XSS_FILTER", "X_FRAME_OPTIONS", "SECURE_SSL_REDIRECT"
+"PORT"
+"STATIC_ROOT"
